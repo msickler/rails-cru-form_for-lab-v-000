ff6398f58b97af72b6dc434ec511601670c75d96
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-11 19:57:53 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,33 @@
 class ArtistsController < ApplicationController
 
+  def new
+   @artist = Artist.new
+ end
+
+ def create
+   @artist = Artist.new(params.require(:artist))
+   @artist.save
+   redirect_to artist_path(@artist)
+ end
+
+ def show
+   @artist = Artist.find(params[:id])
+ end
+
+ def edit
+   @artist = Artist.find(params[:id])
+ end
+
+ def update
+   @artist = Artist.find(params[:id])
+   @artist.update(params.require(:artist))
+   redirect_to artist_path(@artist)
+ end
+
+ private
+
+ def post_params(*args)
+   params.require(:artist).permit(*args)
+ end
+
 end
