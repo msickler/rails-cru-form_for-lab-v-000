c6f027b8f9975cecde538d1f78a9670b993214e4
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-11 19:58:55 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,33 @@
 class SongsController < ApplicationController
 
+  def new
+   @song = Song.new
+ end
+
+ def create
+   @song = Song.new(params.require(:song))
+   @song.save
+   redirect_to song_path(@song)
+ end
+
+ def show
+   @song = Song.find(params[:id])
+ end
+
+ def edit
+   @song = Song.find(params[:id])
+ end
+
+ def update
+   @song = Song.find(params[:id])
+   @song.update(params.require(:song))
+   redirect_to song_path(@song)
+ end
+
+ private
+
+ def post_params(*args)
+   params.require(:song).permit(*args)
+ end
+
 end
