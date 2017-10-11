702dd28cbeaee59356573af5593b72574a587de0
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-11 20:34:10 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,34 +4,31 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
+  def show
+    @song = Song.find_by(id: params[:id])
+  end
+
   def new
-   @song = Song.new
- end
+    @song = Song.new
+  end
 
- def create
-   @song = Song.new(params.require(:song).permit(:name, :artist_id, :genre_id))
-   @song.save
-   redirect_to song_path(@song)
- end
+  def create
+    @song = Song.new(params.require(:song).permit(:name, :artist_id, :genre_id))
+    @song.save
 
- def show
-   @song = Song.find(params[:id])
- end
+    redirect_to song_path(@song)
+  end
 
- def edit
-   @song = Song.find(params[:id])
- end
+  def edit
+    @song = Song.find_by(id: params[:id])
+  end
 
- def update
-   @song = Song.find(params[:id])
-   @song.update(params.require(:song.permit(:name, :artist_id, :genre_id))
-   redirect_to song_path(@song)
- end
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
 
- private
-
- def post_params(*args)
-   params.require(:song).permit(*args)
- end
+    redirect_to song_path(@song)
+  end
+end
 
 end
